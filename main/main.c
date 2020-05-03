220661f3dbb0b031bce2f1ec3428bb1536cff7c8
Ian Wakely <raveious.irw@gmail.com>
2020-05-03 09:15:13 -0400

    integrated example mqtt app into existing one.

--- main/main.c
+++ main/main.c
@@ -1,23 +1,57 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
 #include <string.h>
-#include <stdlib.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+// #include "freertos/semphr.h"
 #include "freertos/queue.h"
+#include "freertos/event_groups.h"
 
-#include "esp_log.h"
 #include "esp_system.h"
-#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+
+#include "nvs.h"
+#include "nvs_flash.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "mqtt_client.h"
 
 #include "driver/i2c.h"
 
 #include "mcp23008_driver.h"
 
+static const char *TAG = "MQTT_EXAMPLE";
+
+static EventGroupHandle_t wifi_event_group;
+const static int CONNECTED_BIT = BIT0;
+
+
 #define I2C_SDA_PIN       0
 #define I2C_SCL_PIN       2
 
 #define DEVICE_MASTER_NUM I2C_NUM_0
+
+typedef struct {
+    uint8_t motion_activated   :1;
+    uint8_t timeout            :3;
+    uint8_t buzzer             :1;
+    uint8_t green_light        :1;
+    uint8_t yellow_light       :1;
+    uint8_t red_light          :1;
+} __attribute__((packed)) message_t;
+
+message_t latest_message;
+bool active_timeout = true;
+
+volatile uint8_t  input_state = 0;
 
 static esp_err_t i2c_init (void) {
     i2c_config_t config;
@@ -34,6 +68,7 @@ static esp_err_t i2c_init (void) {
 
     ESP_ERROR_CHECK(i2c_driver_install(DEVICE_MASTER_NUM, config.mode));
     ESP_ERROR_CHECK(i2c_param_config(DEVICE_MASTER_NUM, &config));
+    
     return ESP_OK;
 }
 
@@ -44,20 +79,170 @@ static void i2c_task(void* arg) {
 
     mcp23008_write_output(0x00);
 
+    uint8_t output_state = 0;
+    uint8_t last_output_state = 0;
+
     while(1) {
-        mcp23008_write_output((1 << 0));
+        // init the variable
+        output_state = 0x0;
+        
+        // Parse the latest update
+        output_state |= (latest_message.red_light     << 0);
+        output_state |= (latest_message.yellow_light  << 1);
+        output_state |= (latest_message.green_light   << 2);
+        output_state |= (latest_message.buzzer        << 3);
+
+        // Only write the update, if it's different than the last one
+        if (output_state != last_output_state) {
+            mcp23008_write_output(output_state);
+            last_output_state = output_state;
+        }
+
+        // Sleep
         vTaskDelay(1000 / portTICK_RATE_MS);
-        mcp23008_write_output((1 << 1));
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        mcp23008_write_output((1 << 2));
-        vTaskDelay(1000 / portTICK_RATE_MS);
+        
+        // Only do the timeout if the timeout is active
+        if (active_timeout) {
+            // Timeout if this was supposed to timeout
+            if (latest_message.timeout == 0) {
+                output_state = 0x0;
+            
+            // Decrement timeout
+            } else {
+                latest_message.timeout--;
+            }
+        }
     }
 
     i2c_driver_delete(DEVICE_MASTER_NUM);
 }
 
-void app_main(void) {
+static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
+{
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    // your_context_t *context = event->context;
+    switch (event->event_id) {
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+            msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+            msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+            ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+            break;
+
+        case MQTT_EVENT_SUBSCRIBED:
+            ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+            msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+            break;
+        case MQTT_EVENT_UNSUBSCRIBED:
+            ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+            break;
+        case MQTT_EVENT_ERROR:
+            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+            break;
+    }
+    return ESP_OK;
+}
+
+static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
+{
+    /* For accessing reason codes in case of disconnection */
+    system_event_info_t *info = &event->event_info;
+    
+    switch (event->event_id) {
+        case SYSTEM_EVENT_STA_START:
+            esp_wifi_connect();
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            ESP_LOGE(TAG, "Disconnect reason : %d", info->disconnected.reason);
+            if (info->disconnected.reason == WIFI_REASON_BASIC_RATE_NOT_SUPPORT) {
+                /*Switch to 802.11 bgn mode */
+                esp_wifi_set_protocol(ESP_IF_WIFI_STA, WIFI_PROTOCAL_11B | WIFI_PROTOCAL_11G | WIFI_PROTOCAL_11N);
+            }
+            esp_wifi_connect();
+            xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
+static void wifi_init(void)
+{
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, NULL));
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_WIFI_SSID,
+            .password = CONFIG_WIFI_PASSWORD,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_LOGI(TAG, "start the WIFI SSID:[%s]", CONFIG_WIFI_SSID);
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_LOGI(TAG, "Waiting for wifi");
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+}
+
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = CONFIG_BROKER_URL,
+        .event_handle = mqtt_event_handler,
+        // .user_context = (void *)your_context
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_start(client);
+}
+
+void app_main()
+{
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
 
     // Make a task just to monitor the I2C device.
     xTaskCreate(i2c_task, "i2c_task", 2048, NULL, 10, NULL);
+
+    nvs_flash_init();
+    wifi_init();
+    mqtt_app_start();
 }
